9a48bea5eddb812d9f4726652605be3cd37ccbb3
plopeziv <plopez@8thlight.com>
2021-10-20 09:53:42 -0500

    Add mix test-watch to project

--- test/elixir_tic_tac_toe_test.exs
+++ test/elixir_tic_tac_toe_test.exs
@@ -1,8 +1,17 @@
 defmodule ElixirTicTacToeTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest ElixirTicTacToe
 
   test "greets the world" do
     assert ElixirTicTacToe.hello() == :world
+  end
+
+  test "Print Board" do
+    # expectedOutput = "\n 1 | 2 | 3 \n---+---+---\n 4 | 5 | 6 \n---+---+---\n 7 | 8 | 9 \n"
+
+
+    # assert capture_io(ElixirTicTacToe.printBoard()) == expectedOutput
+    assert true == true
   end
 end
