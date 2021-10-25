23aab828583646924bbae7f4f0b1ddd1ff026df4
plopeziv <plopez@8thlight.com>
2021-10-25 16:59:09 -0500

    CCreate a input collector & valid input creator

--- lib/get_user_move.ex
+++ lib/get_user_move.ex
@@ -1,12 +1,31 @@
 defmodule GetUserMove do
   def getMove(board, token) do
-    # Prompt user for move
 
-    # Slugify move
+    raw_input = _slugInput()
 
     # Check for Valid input
 
     # Replace appropriate  Space
+  end
+
+  def _slugInput() do
+    returnedInput = IO.gets("Your Turn! Please choose an empty game space. ")
+
+    String.trim(returnedInput)
+  end
+
+  def _createValidInputArray([head|tail], token, recursiveList) do
+    if elem(head, 1) == token or elem(head, 1) == "X" do
+      _createValidInputArray(tail, token, recursiveList)
+
+    else
+      _createValidInputArray(tail, token, [elem(head, 1)| recursiveList])
+    end
+
+  end
+
+  def _createValidInputArray([], token, recursiveList) do
+    recursiveList
   end
 
 end
--- test/get_user_move_test.exs
+++ test/get_user_move_test.exs
@@ -1,6 +1,6 @@
 defmodule GetUserMoveTest do
   use ExUnit.Case, async: false
-  import ExUnit.CaptureIO
+  # import ExUnit.CaptureIO
   import Mock
   doctest GetUserMove
 
@@ -13,16 +13,36 @@ defmodule GetUserMoveTest do
 
     userToken = "T"
 
-    assert GetUserMove.getMove(board, userToken) == [
-      spotOne: "T", spotTwo: "X", spotThree: "T",
-      spotFour: "4", spotFive: "T", spotSix: "6",
-      spotSeven: "7", spotEight: "X", spotNine: "9"
-    ]
+    Mock.with_mock IO, [gets: fn(_prompt) -> "5\n" end] do
+      assert GetUserMove.getMove(board, userToken) == [
+        spotOne: "T", spotTwo: "X", spotThree: "T",
+        spotFour: "4", spotFive: "T", spotSix: "6",
+        spotSeven: "7", spotEight: "X", spotNine: "9"
+      ]
+    end
 
   end
 
-  test_with_mock "Gather User Input", IO, [:passthrough], ["3"] do
+  test "Gather User Input and Slugify" do
+    Mock.with_mock IO, [gets: fn(_prompt) -> "3\n" end] do
+      result = GetUserMove._slugInput()
+      assert result == "3"
+    end
 
+  end
+
+  test "Create a Valid Input Array" do
+    board = [
+      spotOne: "a", spotTwo: "X", spotThree: "a",
+      spotFour: "4", spotFive: "5", spotSix: "6",
+      spotSeven: "7", spotEight: "X", spotNine: "9"
+    ]
+
+    userToken = "a"
+
+    assert GetUserMove._createValidInputArray(board, userToken, []) == [
+      "9", "7", "6", "5", "4"
+    ]
   end
 
 end
