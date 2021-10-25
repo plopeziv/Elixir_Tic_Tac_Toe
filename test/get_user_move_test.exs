490dcec6744fcae9be61137acbf0d531a6b1df19
plopeziv <plopez@8thlight.com>
2021-10-25 15:42:16 -0500

    Wire :mock for CircleCI

--- test/get_user_move_test.exs
+++ test/get_user_move_test.exs
@@ -1,22 +1,27 @@
 defmodule GetUserMoveTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   import ExUnit.CaptureIO
+  import Mock
   doctest GetUserMove
 
   test "Get Valid User Move" do
     board = [
       spotOne: "T", spotTwo: "X", spotThree: "T",
-      spotFour: "4", spotFive: "X", spotSix: "6",
-      spotSeven: "7", spotEight: "8", spotNine: "9"
+      spotFour: "4", spotFive: "5", spotSix: "6",
+      spotSeven: "7", spotEight: "X", spotNine: "9"
     ]
 
     userToken = "T"
 
     assert GetUserMove.getMove(board, userToken) == [
       spotOne: "T", spotTwo: "X", spotThree: "T",
-      spotFour: "4", spotFive: "X", spotSix: "6",
-      spotSeven: "7", spotEight: "T", spotNine: "9"
+      spotFour: "4", spotFive: "T", spotSix: "6",
+      spotSeven: "7", spotEight: "X", spotNine: "9"
     ]
+
+  end
+
+  test_with_mock "Gather User Input", IO, [:passthrough], ["3"] do
 
   end
 
