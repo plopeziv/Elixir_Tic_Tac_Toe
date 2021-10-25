a7ef3197a192b905bd8d010cdda28026332e891a
plopeziv <plopez@8thlight.com>
2021-10-25 10:47:36 -0500

    Creates initial GetUserMove files

---
+++ lib/get_user_move.ex
@@ -0,0 +1,3 @@
+defmodule GetUserMove do
+
+end
---
+++ test/get_user_move_test.exs
@@ -0,0 +1,11 @@
+defmodule GetUserMoveTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+  doctest GetUserMove
+
+  test "Dummy Test" do
+    assert true == true
+
+  end
+
+end
