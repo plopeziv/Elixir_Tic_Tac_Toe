402b78a353620111bba415d7aeb87c7b663d5c04
plopeziv <plopez@8thlight.com>
2021-11-05 09:34:38 -0500

    CConfigure files to accept ecto database

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule ElixirTicTacToe.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {TTT.Application, []}
     ]
   end
 
@@ -24,6 +25,8 @@ defmodule ElixirTicTacToe.MixProject do
     [
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:mock, "~> 0.3.0", only: :test},
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
