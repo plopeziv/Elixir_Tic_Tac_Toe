ecd8d7da23b944970a66ffc8540ba4f791d0bdbd
Matt Readout <41033651+digitopolis@users.noreply.github.com>
2021-11-08 16:31:25 -0800

    Remove unused argument from main/1

--- lib/main.ex
+++ lib/main.ex
@@ -2,7 +2,7 @@ import ElixirTicTacToe
 
 defmodule Main do
 
-  def main(args) do
+  def main() do
     # {opts,_,_}= OptionParser.parse(args,switches: [file: :string],aliases: [f: :file])
     # IO.inspect opts #here I just inspect the options to stdout
     play_TTT([spotOne: "1", spotTwo: "2", spotThree: "3",
