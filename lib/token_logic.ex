c7278e258077d169e17650af2d64edce8f29c739
plopeziv <plopez@8thlight.com>
2021-11-12 10:26:17 -0600

    Add retrieve Token

--- lib/token_logic.ex
+++ lib/token_logic.ex
@@ -1,16 +1,44 @@
 defmodule GetTokens do
-  def testQuery() do
-    [head|_tail] = TTT.Player |> TTT.Repo.all
+  import Ecto.Query
 
-    head.playerName
+
+  def retrieve_token(person) do
+    if _is_player_in_db(person) do
+      token = _query_player_token(person)
+      IO.puts("Welcome #{person}! You will play as #{token}")
+
+      token
+
+    else
+      token = "O"
+      IO.puts("Player not found! User will play as #{token}")
+
+      token
+    end
   end
 
-  def queryPlayerToken(person) do
-    require Ecto.Query
+  def get_player() do
 
-    [head|_tail]= TTT.Player |> Ecto.Query.where(playerName: ^person) |> TTT.Repo.all
+  end
+
+  def _slug_input(message_prompt) do
+    input = IO.gets(message_prompt)
+    if input == :eof do
+      raise "End of File! Check for propper mocks and imports"
+    else
+      String.trim(input)
+    end
+  end
+
+  def _query_player_token(person) do
+    [head|_tail]= TTT.Player |> where(playerName: ^person) |> TTT.Repo.all
 
     head.token
   end
 
+  def _is_player_in_db(person) do
+    query = from player in TTT.Player, where: player.playerName == ^person
+
+    TTT.Repo.exists?(query)
+  end
 end
--- test/token_logic_test.exs
+++ test/token_logic_test.exs
@@ -1,16 +1,47 @@
 defmodule GetTokensTest do
   use ExUnit.Case, async: false
-  # aimport ExUnit.CaptureIO
-  # import Mock
+  import ExUnit.CaptureIO
+  import Mock
   doctest GetTokens
 
-  test "First Query" do
-
-    assert GetTokens.testQuery() == "Pedro"
+  test "Retrieve Token gives token of an available player" do
+    assert GetTokens.retrieve_token("Dave")  == "D"
   end
 
+  test "Retrieve Token welcomes user" do
+    assert capture_io(fn -> GetTokens.retrieve_token("Dave")
+      end) == "Welcome Dave! You will play as D\n"
+  end
+
+  test "Retrieve Token returns O as spot" do
+    assert GetTokens.retrieve_token("Stacey") == "O"
+  end
+
+  test "Retriev Token prompts user as O" do
+    assert capture_io(fn -> GetTokens.retrieve_token("Stacey")
+      end) == "Player not found! User will play as O\n"
+  end
+
+  test "Gather User Input and Slugify" do
+    Mock.with_mock IO, [gets: fn(_prompt) -> "3\n" end] do
+      result = GetUserMove._slug_input("This is a test string")
+      assert result == "3"
+    end
+
+  end
+
+  # Query Code Starts Here This Code will be mocked and  injected for above
+
   test "Query for Player" do
-    assert GetTokens.queryPlayerToken("Matt") == "M"
+    assert GetTokens._query_player_token("Matt") == "M"
+  end
+
+  test "Is player in database returns True" do
+    assert GetTokens._is_player_in_db("Pedro") == true
+  end
+
+  test "Is player in database returns False" do
+    assert GetTokens._is_player_in_db("Sunny") == false
   end
 
 end
