37e486b3e2c8b2c3f57b6c8aedd3c4ca8d508a7f
plopeziv <plopez@8thlight.com>
2021-11-12 18:43:40 -0600

    Create take_spot_five

---
+++ test/SpecialCombos_test.exs
@@ -0,0 +1,47 @@
+defmodule SpecialCombosTest do
+  use ExUnit.Case
+  doctest SpecialCombos
+
+  test "Spot Five gets taken if Empty" do
+    board = [
+      spotOne: "T", spotTwo: "X", spotThree: "T",
+      spotFour: "4", spotFive: "5", spotSix: "6",
+      spotSeven: "7", spotEight: "X", spotNine: "9"
+    ]
+
+    expected_outcome = [
+        spotOne: "T", spotTwo: "X", spotThree: "T",
+        spotFour: "4", spotFive: "X", spotSix: "6",
+        spotSeven: "7", spotEight: "X", spotNine: "9"
+      ]
+
+    user_token = "T"
+
+    assert SpecialCombos.take_spot_five(board, user_token) == expected_outcome
+  end
+
+  test "take_spot_five does nothing if spotFive is taken by computer" do
+    board = [
+      spotOne: "T", spotTwo: "2", spotThree: "T",
+      spotFour: "4", spotFive: "X", spotSix: "6",
+      spotSeven: "7", spotEight: "X", spotNine: "9"
+    ]
+
+    user_token = "T"
+
+    assert SpecialCombos.take_spot_five(board, user_token) == board
+  end
+
+  test "take_spot_five does nothing if spotFive is taken by user" do
+    board = [
+      spotOne: "T", spotTwo: "2", spotThree: "T",
+      spotFour: "4", spotFive: "T", spotSix: "6",
+      spotSeven: "7", spotEight: "X", spotNine: "9"
+    ]
+
+    user_token = "T"
+
+    assert SpecialCombos.take_spot_five(board, user_token) == board
+  end
+
+end
