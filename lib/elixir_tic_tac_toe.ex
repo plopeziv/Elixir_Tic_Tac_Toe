b297ae04d325461a86789bf3f6b3957bdd39db17
plopeziv <plopez@8thlight.com>
2021-11-16 10:40:07 -0600

    Add Mix.Task.run to run app

--- lib/elixir_tic_tac_toe.ex
+++ lib/elixir_tic_tac_toe.ex
@@ -7,8 +7,8 @@ defmodule ElixirTicTacToe do
 
   def play_TTT(board) do
 
-    # _game_loop(board, return_token())
-    _game_loop(board, "A")
+    _game_loop(board, return_token())
+    # _game_loop(board, "A")
 
   end
 
