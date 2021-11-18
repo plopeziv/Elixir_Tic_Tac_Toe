efa7431a3737989798f0d707e3944f61853f5261
plopeziv <plopez@8thlight.com>
2021-11-18 16:13:41 -0600

    Utilize function capturing for get_best_spot

--- lib/best_computer_spot.ex
+++ lib/best_computer_spot.ex
@@ -5,20 +5,14 @@ defmodule BestComputerSpot do
 
   def get_best_spot(board, token)do
 
-    Enum.reduce_while(_assign_priority_list(), [], fn (function, accum) ->
-      if board != function.(board,  token),
-      do: {:halt, accum ++ function.(board, token)},
-      else: {:cont, accum}
+    Enum.reduce_while(
+      [&win_game/2, &defend_loss/2,
+        &take_spot_five/2, &take_first_available_spot/2],
+      [],
+      fn (function, accum) ->
+        if board != function.(board,  token),
+        do: {:halt, accum ++ function.(board, token)},
+        else: {:cont, accum}
     end)
-
-  end
-
-  def _assign_priority_list() do
-    priority1 =  fn (board, token) -> win_game(board, token) end
-    priority2 =  fn (board, token) -> defend_loss(board, token) end
-    priority3 =  fn (board, token) -> take_spot_five(board, token) end
-    priority4 =  fn (board, token) -> take_first_available_spot(board,  token) end
-
-    [priority1, priority2, priority3,  priority4]
   end
 end
