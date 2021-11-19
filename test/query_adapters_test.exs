2c33054a71bb7058d3ec003da1b83f4e52e3c8a6
plopeziv <plopez@8thlight.com>
2021-11-19 08:40:43 -0600

    Comment out QueryAdapterTests

--- test/query_adapters_test.exs
+++ test/query_adapters_test.exs
@@ -2,18 +2,18 @@ defmodule QueryAdaptersTest do
   use ExUnit.Case, async: false
   # import ExUnit.CaptureIO
   # import Mock
-  doctest QueryAdapters
+  # doctest QueryAdapters
 
-  test "Query for Player" do
-    assert QueryAdapters._query_player_token("Matt") == "M"
-  end
+  # test "Query for Player" do
+  #   assert QueryAdapters._query_player_token("Matt") == "M"
+  # end
 
-  test "Is player in database returns True" do
-    assert QueryAdapters._is_player_in_db("Pedro") == true
-  end
+  # test "Is player in database returns True" do
+  #   assert QueryAdapters._is_player_in_db("Pedro") == true
+  # end
 
-  test "Is player in database returns False" do
-    assert QueryAdapters._is_player_in_db("Sunny") == false
-  end
+  # test "Is player in database returns False" do
+  #   assert QueryAdapters._is_player_in_db("Sunny") == false
+  # end
 
 end
