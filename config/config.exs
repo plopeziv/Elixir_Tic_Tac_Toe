7910b0d8797c1f2ff0355dd828bd2a46f3f3faea
plopeziv <plopez@8thlight.com>
2021-11-23 11:10:10 -0600

    Remove pools configuration

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,6 @@ import Config
 
 config :elixir_tic_tac_toe, TTT.Repo,
   database: "elixir_tic_tac_toe_repo",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  hostname: "localhost"
 
 config :elixir_tic_tac_toe, ecto_repos: [TTT.Repo]
--- config/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :elixir_tic_tac_toe, TTT.Repo,
-  database: "elixir_tic_tac_toe_repo",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
-import_config "#{Mix.env()}.exs"
