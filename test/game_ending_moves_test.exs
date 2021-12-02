e116333c0b5f26466873bfda0d0003fac1efdd86
plopeziv <plopez@8thlight.com>
2021-12-01 19:44:39 -0600

    Add first computer spot to GameEndingMoves

--- test/game_ending_moves_test.exs
+++ test/game_ending_moves_test.exs
@@ -1,5 +1,6 @@
 defmodule GameEndingMovesTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case
+  import ExUnit.CaptureIO
   doctest GameEndingMoves
 
   test "Computer should win game" do
@@ -94,6 +95,51 @@ defmodule GameEndingMovesTest do
     user_token = "T"
 
     assert GameEndingMoves.take_spot_five(board, user_token) == board
+  end
+
+  test "Computer Takes First Available Spot" do
+    board = [
+      spotOne: "O", spotTwo: "2", spotThree: "3",
+      spotFour: "4", spotFive: "5", spotSix: "6",
+      spotSeven: "X", spotEight: "8", spotNine: "O"
+    ]
+
+    user_token ="O"
+
+    assert GameEndingMoves.take_first_available_spot(board, user_token) == [
+      spotOne: "O", spotTwo: "X", spotThree: "3",
+      spotFour: "4", spotFive: "5", spotSix: "6",
+      spotSeven: "X", spotEight: "8", spotNine: "O"
+    ]
+
+  end
+
+  test "Replace First Spot Returns New Board" do
+    board = [
+      spotOne: "O", spotTwo: "X", spotThree: "O",
+      spotFour: "4", spotFive: "5", spotSix: "6",
+      spotSeven: "7", spotEight: "8", spotNine: "9"
+    ]
+
+    user_token = "O"
+
+    assert GameEndingMoves._replace_first_spot(board, user_token, []) == [
+      spotOne: "O", spotTwo: "X", spotThree: "O",
+      spotFour: "X", spotFive: "5", spotSix: "6",
+      spotSeven: "7", spotEight: "8", spotNine: "9"
+    ]
+  end
+
+  test "Replace First Spot Realizes Cats Game" do
+    board = [
+      spotOne: "O", spotTwo: "X", spotThree: "O",
+      spotFour: "X", spotFive: "X", spotSix: "O",
+      spotSeven: "X", spotEight: "O", spotNine: "X"
+    ]
+
+    user_token = "O"
+
+    assert capture_io(fn -> GameEndingMoves._replace_first_spot(board, user_token, []) end) == "Cat's Game!\n"
   end
 
   test "Function will replace first diffence in a key-pair list" do
